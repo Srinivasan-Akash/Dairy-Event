63f2ce14fd1b5618ee8118b63fc61b17e6131e31
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-08-21 15:26:04 +0530

    Fixed minor issue with meeting links

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import "./dashboard.scss"
+import Logo from "../assets/home page/logo.png";
+import Pointer from "../assets/home page/pointer.png";
+import { Settings, Trash2 } from "lucide-react";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+
+      <div className="gradient"></div>
+      <nav>
+        <img src={Logo} alt="Logo" className="logo" />
+        
+        <div className="right-nav">
+          <input type="text" placeholder="Search For Your Event" />
+          <Link to={"/dashboard"}>
+          <button>
+          <img src={Pointer} alt="Pointer" />
+            Create Event
+          </button>
+        </Link>
+        </div>
+      </nav>
+
+      <div className="grid">
+        <div className="card">
+          <h2>7th Aug - 8th Aug</h2>
+          <h1>Postman Dev Meetup</h1> 
+          <p>A specialized meeting calling out all designers and developers from India, USA, France, Canada for an important & crucial.</p>
+          <div className="btns">
+          <button>Copy Event Link</button>
+          <button><Settings /></button>
+          <button><Trash2 /></button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
